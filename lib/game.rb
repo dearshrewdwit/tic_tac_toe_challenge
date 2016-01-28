44ea7b3171d24f52e167250627c555cd9e417914
dearshrewdwit <dearshrewdwit@gmail.com>
2016-01-28 15:26:10 +0000

    guard conditions for insert, #finished?

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,26 @@
 class Game
-  DEFAULT = 3
+
   attr_reader :grid
-  def initialize(size=DEFAULT)
+
+  def initialize
     @grid = [ [nil,nil,nil],
               [nil,nil,nil],
               [nil,nil,nil] ]
   end
+
+  def insert(player_symbol, row, column)
+    raise "choose a number between 0-2" if invalid_number?(row,column)
+    grid[row][column] = player_symbol
+  end
+
+  def finished?
+    !grid.any? { |row| row.include?(nil) }
+  end
+
+  private
+
+  def invalid_number?(row, column)
+    row < 0 || column < 0 || row > 2 || column > 2
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,9 +3,50 @@ require 'game'
 describe Game do
 
   subject(:game) { described_class.new }
+  let(:player1_symbol) { :x }
+  let(:player1_symbol) { :o }
 
   it "initializes with a 3x3 grid" do
     expect(game.grid.count).to eq 3
+  end
+
+  it "can modify certain elements" do
+    game.insert(player1_symbol, 0, 0)
+    expect(game.grid[0][0]).to eq player1_symbol
+  end
+
+  it "can modify certain elements" do
+    game.insert(player1_symbol, 1, 1)
+    expect(game.grid[1][1]).to eq player1_symbol
+  end
+
+  it "can modify certain elements" do
+    game.insert(player1_symbol, 2, 2)
+    expect(game.grid[2][2]).to eq player1_symbol
+  end
+
+  it "raises error if invalid number" do
+    expect {game.insert(player1_symbol, 2, 3)}.to raise_error "choose a number between 0-2"
+    expect {game.insert(player1_symbol, 3, 2)}.to raise_error "choose a number between 0-2"
+    expect {game.insert(player1_symbol, -1, 2)}.to raise_error "choose a number between 0-2"
+    expect {game.insert(player1_symbol, 2, -1)}.to raise_error "choose a number between 0-2"
+  end
+
+  it "finished? on an incomplete game returns false" do
+    expect(game.finished?).to be false
+  end
+
+  it "can return true when all spots are filled" do
+    game.insert(player1_symbol, 0, 0)
+    game.insert(player1_symbol, 0, 1)
+    game.insert(player1_symbol, 0, 2)
+    game.insert(player1_symbol, 1, 0)
+    game.insert(player1_symbol, 1, 1)
+    game.insert(player1_symbol, 1, 2)
+    game.insert(player1_symbol, 2, 0)
+    game.insert(player1_symbol, 2, 1)
+    game.insert(player1_symbol, 2, 2)
+    expect(game.finished?).to be true
   end
 
 end
